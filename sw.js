e75e1d8e1533ea85520289ab964c73327775423c
tamas.czerjak <tamas.czerjak@icellmobilsoft.hu>
2018-06-18 12:37:41 +0200

    feat: clear old cache on activing of the service worker

--- sw.js
+++ sw.js
@@ -1,28 +1,45 @@
+const CACHE = 'mws-restaurant-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/restaurant.html',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches
-      .open('mws-restaurant-v1')
-      .then(cache => cache.addAll([
-        '/',
-        '/css/styles.css',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        '/restaurant.html',
-        '/data/restaurants.json',
-        '/img/1.jpg',
-        '/img/2.jpg',
-        '/img/3.jpg',
-        '/img/4.jpg',
-        '/img/5.jpg',
-        '/img/6.jpg',
-        '/img/7.jpg',
-        '/img/8.jpg',
-        '/img/9.jpg',
-        '/img/10.jpg',
-      ]))
+      .open(CACHE)
+      .then(cache => cache.addAll(STATIC_ASSETS))
       .catch(err => console.warn('Static assets cannot be added to cache: ', err))
       .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restaurant-') && !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
   );
 });
 
@@ -32,7 +49,9 @@ self.addEventListener('fetch', event => {
       .match(event.request, { ignoreSearch: true })
       .then(response => {
         // console.log(response);
-        return !!response ? response : fetch(event.request);
+        return !!response ? response :
+          fetch(event.request)
+          .catch(err => console.warn('Fetching has fas failed: ', err));
       })
   );
 });
