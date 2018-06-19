c9387f321734b54807877bae48dc3f505a3613b6
tamas.czerjak <tamas.czerjak@icellmobilsoft.hu>
2018-06-19 12:41:58 +0200

    feat: add dynamic alt attributes to the image of restaurant detail page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -57,6 +57,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
+  image.alt = `image of restaurant ${ restaurant.name}`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
