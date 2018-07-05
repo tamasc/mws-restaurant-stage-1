f40fc3e2f410392045594b37be50b03c3a85f438
tamas.czerjak <tamas.czerjak@icellmobilsoft.hu>
2018-07-05 19:13:34 +0200

    feat: mark favourite restaurant at the list

--- js/main.js
+++ js/main.js
@@ -174,24 +174,37 @@ const createRestaurantHTML = (restaurant) => {
   image.classList.add('restaurant-img', 'lozad');
   image.setAttribute('data-src', imgSource);
   image.alt = `image of restaurant ${restaurant.name}`;
-  li.append(image);
+
+  const nameContainer = document.createElement('div');
+  nameContainer.classList.add('name-container');
 
   const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
-  li.append(name);
+
+  const favouriteIcon = document.createElement('span');
+  favouriteIcon.setAttribute('role', 'img');
+  favouriteIcon.classList.add('favourite-icon');
+  if (restaurant.s_favorite) {
+    favouriteIcon.setAttribute('aria-label', 'Remove from favourites');
+    favouriteIcon.innerHTML = '&#9733;';
+  } else {
+    favouriteIcon.setAttribute('aria-label', 'Add to favourites');
+    favouriteIcon.innerHTML = '&#9734;';
+  }
+
+  nameContainer.append(name, favouriteIcon);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+
+  li.append(image, nameContainer, neighborhood, address, more)
 
   return li
 }
