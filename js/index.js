7805ed67f81184679462075f94630e64525e4fed
tamas.czerjak <tamas.czerjak@icellmobilsoft.hu>
2018-07-22 23:55:08 +0200

    feat: show notification when offline

--- js/index.js
+++ js/index.js
@@ -14,3 +14,20 @@ if ('serviceWorker' in navigator) {
         }
     });
 }
+
+window.addEventListener('offline', e => {
+    const notification = document.getElementById('statusNotification');
+    notification.innerText = 'The application is offline!';
+    notification.setAttribute('ariaHidden', false);
+
+    notification.classList.add('offline');
+});
+window.addEventListener('online', e => {
+    const notification = document.getElementById('statusNotification');
+    notification.innerText = 'The application is online!';
+    setTimeout(() => {
+        notification.setAttribute('ariaHidden', true);
+    }, 10*1000);
+
+    notification.classList.remove('offline');
+});
